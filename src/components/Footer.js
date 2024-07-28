093d19331c67f10093a55a01c3e4d00989e0d566
Srainyyyy <s223733764@deakin.edu.au>
2024-07-28 14:28:48 +1000

    Home Page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,87 +1,43 @@
 import React from 'react';
-import { Segment, Grid, List, Header, Icon } from 'semantic-ui-react';
+import { Icon,Image } from 'semantic-ui-react';
 import './Footer.css'; // 引入 CSS 文件
 
 const Footer = () => (
-    <Segment inverted vertical className='footer-segment' style={{ padding: '2em 0em' }}>
-        <Grid divided inverted stackable>
-            <Grid.Row>
-                <Grid.Column width={5}>
-                    <Header inverted as='h4' content='Explore' />
-                    <Grid>
-                        <Grid.Row columns={2}>
-                            <Grid.Column>
-                                <List link inverted>
-                                    <List.Item as='a'>Home</List.Item>
-                                </List>
-                            </Grid.Column>
-                            <Grid.Column>
-                                <List link inverted>
-                                    <List.Item as='a'>Questions</List.Item>
-                                </List>
-                            </Grid.Column>
-                            <Grid.Column>
-                                <List link inverted>
-                                    <List.Item as='a'>Articles</List.Item>
-
-                                </List>
-                            </Grid.Column>
-                            <Grid.Column>
-                                <List link inverted>
-                                    <List.Item as='a'>Tutorials</List.Item>
-                                </List>
-                            </Grid.Column>
-                        </Grid.Row>
-                    </Grid>
-                </Grid.Column>
-                <Grid.Column width={5}>
-                    <Header inverted as='h4' content='Support' />
-                    <Grid>
-                        <Grid.Row columns={2}>
-                            <Grid.Column>
-                                <List link inverted>
-                                    <List.Item as='a'>FAQs</List.Item>
-
-                                </List>
-                            </Grid.Column>
-                            <Grid.Column>
-                                <List link inverted>
-                                    <List.Item as='a'>Help</List.Item>
-                                </List>
-                            </Grid.Column>
-                            <Grid.Column>
-                                <List link inverted>
-                                    <List.Item as='a'>Contact Us</List.Item>
-                                </List>
-                            </Grid.Column>
-                        </Grid.Row> 
-                    </Grid>
-                </Grid.Column>
-                <Grid.Column width={6}>
-                    <Header inverted as='h4' content='Stay connected' />
-                    <List link inverted>
-                        <List.Item as='a'>
-                            <Icon name='facebook' /> Facebook
-                        </List.Item>
-                        <List.Item as='a'>
-                            <Icon name='twitter' /> Twitter
-                        </List.Item>
-                        <List.Item as='a'>
-                            <Icon name='instagram' /> Instagram
-                        </List.Item>
-                    </List>
-                </Grid.Column>
-            </Grid.Row>
-        </Grid>
-        <div style={{ textAlign: 'center', marginTop: '2em' }}>
-            <p>© DEV@Deakin 2022</p>
-            <List horizontal inverted divided link size='small' className='horizontal-list'>
-                <List.Item as='a' href='#'>Privacy Policy</List.Item>
-                <List.Item as='a' href='#'>Terms</List.Item>
-                <List.Item as='a' href='#'>Code of Conduct</List.Item>
-            </List>
+    <div className='footer-container'>
+        <div className='footer-column'>
+            <h4>Explore</h4>
+            <ul>
+                <li>Home</li>
+                <li>Questions</li>
+                <li><a href='src\components\FeaturedArticles.js'>Articles</a></li>
+                <li><a href='src\components\Tutorials.js'>Tutorials</a></li>
+            </ul>
         </div>
-    </Segment>
+        <div className='footer-column'>
+            <h4>Support</h4>
+            <ul>
+                <li>FAQs</li>
+                <li>Help</li>
+                <li>Contact Us</li>
+            </ul>
+        </div>
+        <div className='footer-column'>
+            <h4>Stay connected</h4>
+            <ul>
+                <Icon name='facebook' /> <Image src='/image/facebook.png' size='mini' />
+                <Icon name='twitter' /> <Image src='/image/twitter.png' size='mini' />
+               <Icon name='instagram' /> <Image src='/image/instagram.png' size='mini' />
+            </ul>
+        </div>
+        <div className='footer-bottom'>
+            <p>© DEV@Deakin 2022</p>
+            <ul className='horizontal-list'>
+                <li>Privacy Policy</li>
+                <li>Terms</li>
+                <li>Code of Conduct</li>
+            </ul>
+        </div>
+    </div>
 );
 
 export default Footer;
--- src/components/MainImage.js
+++ src/components/MainImage.js
@@ -3,7 +3,7 @@ import { Image } from 'semantic-ui-react';
 import './MainImage.css'; // 或者 './App.css'
 
 const MainImage = () => (
-  <Image className="main-image" src='/image.png' alt='Main' fluid />
+  <Image className="main-image" src='/image/image.png' alt='Main' fluid />
 );
 
 export default MainImage;
